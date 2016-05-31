272422ebc44f18cc9e3d99cfbd2272d91a6057f6
PaquitoSoft <paquitosoftware@gmail.com>
2016-06-01 01:08:30 +0200

    Configure app runtime environment

--- src/js/config/app-config.js
+++ src/js/config/app-config.js
@@ -1,5 +1,12 @@
 
-export default Object.freeze({
+const developmentConfig = Object.freeze({
 	apiHost: 'http://localhost:3003', // https://small-shop-api.herokuapp.com
 	staticContentBasePath: 'http://localhost:3003/static' // https://dl.dropboxusercontent.com/u/30617876/small-shop
 });
+
+const productionConfig = Object.freeze({
+	apiHost: 'https://small-shop-api.herokuapp.com',
+	staticContentBasePath: 'https://dl.dropboxusercontent.com/u/30617876/small-shop'
+});
+
+export default process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
