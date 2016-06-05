f46850ef3f12b0995451eeb0d567f4955c150850
PaquitoSoft <paquitosoftware@gmail.com>
2016-06-05 11:03:38 +0200

    New production hosting for images

--- src/js/config/app-config.js
+++ src/js/config/app-config.js
@@ -1,12 +1,13 @@
 
 const developmentConfig = Object.freeze({
-	apiHost: 'http://localhost:3003', // https://small-shop-api.herokuapp.com
-	staticContentBasePath: 'http://localhost:3003/static' // https://dl.dropboxusercontent.com/u/30617876/small-shop
+	apiHost: 'http://localhost:3003',
+	staticContentBasePath: 'http://localhost:3003/static'
 });
 
 const productionConfig = Object.freeze({
 	apiHost: 'https://small-shop-api.herokuapp.com',
-	staticContentBasePath: 'https://dl.dropboxusercontent.com/u/30617876/small-shop'
+	_staticContentBasePath: 'https://dl.dropboxusercontent.com/u/30617876/small-shop',
+	staticContentBasePath: 'http://palmist-tapir-66487.bitballoon.com'
 });
 
 export default process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
